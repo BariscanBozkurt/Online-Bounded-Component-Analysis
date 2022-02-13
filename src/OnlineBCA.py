27f33a38c353d733d2e67c836dd8469268ab994f
BariscanBozkurt <bbozkurt15@ku.edu.tr>
2022-02-13 13:22:37 +0300

    nnantisparse model added

--- src/OnlineBCA.py
+++ src/OnlineBCA.py
@@ -22,6 +22,8 @@ from numba import njit, jit
 from tqdm import tqdm
 from IPython.display import display, Latex, Math, clear_output
 from IPython import display as display1
+import warnings
+warnings.filterwarnings("ignore")
 
 class OnlineBCA:
     """
@@ -144,18 +146,36 @@ class OnlineBCA:
         def ProjectOntoLInfty(X, thresh = 1.0):
             return X*(X>=-thresh)*(X<=thresh)+(X>thresh)*thresh-thresh*(X<-thresh)
 
+        yke = np.dot(F, x)
         for j in range(neural_dynamic_iterations):
             mu_y = mu_y_start / (j+1)
             y_old = y.copy()
-            e = np.dot(F, x) - y
+            e = yke - y
             y = y + mu_y*(gamma_hat * B @ y + beta * e)
             y = ProjectOntoLInfty(y)
 
-            if np.linalg.norm(y - y_old) < neural_OUTPUT_COMP_TOL:
+            if np.linalg.norm(y - y_old) < neural_OUTPUT_COMP_TOL * np.linalg.norm(y):
                 break
         return y
 
-        
+    @staticmethod
+    @njit
+    def run_neural_dynamics_nnantisparse(x, y, F, B, beta, gamma_hat, mu_y_start = 0.9, neural_dynamic_iterations = 100, neural_OUTPUT_COMP_TOL = 1e-7):
+        def ProjectOntoNNLInfty(X, thresh = 1.0):
+            return X*(X>=0.0)*(X<=thresh)+(X>thresh)*thresh #-thresh*(X<-thresh)
+
+        yke = np.dot(F, x)
+        for j in range(neural_dynamic_iterations):
+            mu_y = mu_y_start / (j+1)
+            y_old = y.copy()
+            e = yke - y
+            y = y + mu_y*(gamma_hat * B @ y + beta * e)
+            y = ProjectOntoNNLInfty(y)
+
+            if np.linalg.norm(y - y_old) < neural_OUTPUT_COMP_TOL * np.linalg.norm(y):
+                break
+        return y
+
     def compute_overall_mapping(self, return_mapping = False):
         F, B, gamma_hat, beta = self.F, self.B, self.gamma_hat, self.beta
         W = np.linalg.pinv((gamma_hat/beta) * B - np.eye(self.s_dim)) @ F
@@ -260,7 +280,78 @@ class OnlineBCA:
         self.F = F
         self.B = B
         
-        # return Y
+    def fit_batch_nnantisparse(self, X, n_epochs = 2, neural_dynamic_iterations = 250, lr_start = 0.9, whiten = False, whiten_type = 2, shuffle = False, verbose = True, debug_iteration_point = 1000, plot_in_jupyter = False):
+        
+        lambda_, beta, mu_F, gamma_hat, F, B = self.lambda_, self.beta, self.mu_F, self.gamma_hat, self.F, self.B
+        neural_dynamic_tol = self.neural_OUTPUT_COMP_TOL
+        debugging = self.set_ground_truth
+
+        assert X.shape[0] == self.x_dim, "You must input the transpose"
+        
+        samples = X.shape[1]
+        
+        if debugging:
+            SIRlist = []
+            S = self.S
+            A = self.A
+
+        # Y = np.zeros((self.s_dim, samples))
+        Y = np.random.randn(self.s_dim, samples)
+        
+        
+        if shuffle:
+            idx = np.random.permutation(samples) # random permutation
+        else:
+            idx = np.arange(samples)
+            
+        if whiten:
+            X_white, W_pre = self.whiten_signal(X.T, type_ = whiten_type)
+            X_white = X_white.T
+            A = W_pre @ A
+            self.A = A
+        else:
+            X_white = X 
+            
+            
+        for k in range(n_epochs):
+
+            for i_sample in tqdm(range(samples)):
+                x_current = X_white[:,idx[i_sample]]
+                y = np.zeros(self.s_dim)
+
+                y = self.run_neural_dynamics_nnantisparse(x_current, y, F, B, beta, gamma_hat, 
+                                                        mu_y_start = lr_start, neural_dynamic_iterations = neural_dynamic_iterations, 
+                                                        neural_OUTPUT_COMP_TOL = neural_dynamic_tol)
+                        
+                e = F @ x_current - y
+
+                F = F - mu_F * beta * np.outer(e, x_current)
+                
+                z = B @ y
+                B = (1/lambda_) * (B - gamma_hat * np.outer(z, z))
+                # Record the seperated signal
+                Y[:,idx[i_sample]] = y
+
+                if debugging:
+                    if (i_sample % debug_iteration_point) == 0:
+                        self.F = F
+                        self.B = B
+                        W = self.compute_overall_mapping(return_mapping = True)
+                        SIR = self.CalculateSIR(A, W)[0]
+                        SIRlist.append(SIR)
+                        self.SIR_list = SIRlist
+
+                        if plot_in_jupyter:
+                            pl.clf()
+                            pl.plot(np.array(SIRlist), linewidth = 3)
+                            pl.xlabel("Number of Iterations / {}".format(debug_iteration_point), fontsize = 15)
+                            pl.ylabel("SIR (dB)", fontsize = 15)
+                            pl.title("SIR Behaviour", fontsize = 15)
+                            pl.grid()
+                            clear_output(wait=True)
+                            display(pl.gcf())   
+        self.F = F
+        self.B = B
 
 def whiten_signal(X, mean_normalize = True, type_ = 3):
     """
@@ -295,6 +386,37 @@ def whiten_signal(X, mean_normalize = True, type_ = 3):
     
     return X_white, W_pre
 
+def whiten_input(X, n_components = None, return_prewhitening_matrix = False):
+    """
+    X.shape[0] = Number of sources
+    X.shape[1] = Number of samples for each signal
+    """
+    x_dim = X.shape[0]
+    if n_components is None:
+        n_components = x_dim
+    s_dim = n_components
+    
+    N = X.shape[1]
+    # Mean of the mixtures
+    mX = np.mean(X, axis = 1).reshape((x_dim, 1))
+    # Covariance of Mixtures
+    Rxx = np.dot(X, X.T)/N - np.dot(mX, mX.T)
+    # Eigenvalue Decomposition
+    d, V = np.linalg.eig(Rxx)
+    D = np.diag(d)
+    # Sorting indexis for eigenvalues from large to small
+    ie = np.argsort(-d)
+    # Inverse square root of eigenvalues
+    ddinv = 1/np.sqrt(d[ie[:s_dim]])
+    # Pre-whitening matrix
+    Wpre = np.dot(np.diag(ddinv), V[:, ie[:s_dim]].T)#*np.sqrt(12)
+    # Whitened mixtures
+    H = np.dot(Wpre, X)
+    if return_prewhitening_matrix:
+        return H, Wpre
+    else:
+        return H
+        
 def ZeroOneNormalizeData(data):
     return (data - np.min(data)) / (np.max(data) - np.min(data))
 
@@ -305,14 +427,21 @@ def ZeroOneNormalizeColumns(X):
 
     return X_normalized
 
-def Subplot_gray_images(I, image_shape = [512,512], height = 15, width = 15):
-    
+def ProjectOntoLInfty(X, thresh = 1.0):
+    return X*(X>=-thresh)*(X<=thresh)+(X>thresh)*thresh-thresh*(X<-thresh)
+
+def ProjectOntoNNLInfty(X, thresh = 1.0):
+    return X*(X>=0)*(X<=thresh)+(X>thresh)*thresh
+
+def Subplot_gray_images(I, image_shape = [512,512], height = 15, width = 15, title = ''):
     n_images = I.shape[1]
     fig, ax = plt.subplots(1,n_images)
+    fig.suptitle(title)
     fig.set_figheight(height)
     fig.set_figwidth(width)
     for i in range(n_images):
         ax[i].imshow(I[:,i].reshape(image_shape[0],image_shape[1]), cmap = 'gray')
+    
     plt.show()
 
 def subplot_1D_signals(X, title = '',title_fontsize = 20, figsize = (10,5), linewidth = 1, colorcode = '#050C12'):
@@ -360,6 +489,154 @@ def signed_and_permutation_corrected_sources(S,Y):
     perm = find_permutation_between_source_and_estimation(S,Y)
     return np.sign((Y[:,perm] * S).sum(axis = 0)) * Y[:,perm]
 
+def psnr(img1, img2, pixel_max = 1):
+    """
+    Return peak-signal-to-noise-ratio between given two images
+    """
+    mse = np.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    else:
+        return 20 * np.log10(pixel_max / np.sqrt(mse))
+
+def snr(S_original, S_noisy):
+    N_hat = S_original - S_noisy
+    N_P = (N_hat ** 2).sum(axis = 0)
+    S_P = (S_original ** 2).sum(axis = 0)
+    snr = 10 * np.log10(S_P / N_P)
+    return snr
+
+def ProjectRowstoL1NormBall(H):
+    Hshape=H.shape
+    #lr=np.ones((Hshape[0],1))@np.reshape((1/np.linspace(1,Hshape[1],Hshape[1])),(1,Hshape[1]))
+    lr=np.tile(np.reshape((1/np.linspace(1,Hshape[1],Hshape[1])),(1,Hshape[1])),(Hshape[0],1))
+    #Hnorm1=np.reshape(np.sum(np.abs(self.H),axis=1),(Hshape[0],1))
+
+    u=-np.sort(-np.abs(H),axis=1)
+    sv=np.cumsum(u,axis=1)
+    q=np.where(u>((sv-1)*lr),np.tile(np.reshape((np.linspace(1,Hshape[1],Hshape[1])-1),(1,Hshape[1])),(Hshape[0],1)),np.zeros((Hshape[0],Hshape[1])))
+    rho=np.max(q,axis=1)
+    rho=rho.astype(int)
+    lindex=np.linspace(1,Hshape[0],Hshape[0])-1
+    lindex=lindex.astype(int)
+    theta=np.maximum(0,np.reshape((sv[tuple([lindex,rho])]-1)/(rho+1),(Hshape[0],1)))
+    ww=np.abs(H)-theta
+    H=np.sign(H)*(ww>0)*ww
+    return H
+
+def display_matrix(array):
+    data = ''
+    for line in array:
+        if len(line) == 1:
+            data += ' %.3f &' % line + r' \\\n'
+            continue
+        for element in line:
+            data += ' %.3f &' % element
+        data += r' \\' + '\n'
+    display(Math('\\begin{bmatrix} \n%s\end{bmatrix}' % data))
+
+# Calculate SIR Function
+def CalculateSIR(H,pH, return_db = True):
+    G=pH@H
+    Gmax=np.diag(np.max(abs(G),axis=1))
+    P=1.0*((np.linalg.inv((Gmax))@np.abs(G))>0.99)
+    T=G@P.T
+    rankP=np.linalg.matrix_rank(P)
+    diagT = np.diag(T)
+    # Signal Power
+    sigpow = np.linalg.norm(diagT,2)**2
+    # Interference Power
+    intpow = np.linalg.norm(T,'fro')**2 - sigpow
+    SIRV = sigpow/intpow
+    # SIRV=np.linalg.norm((np.diag(T)))**2/(np.linalg.norm(T,'fro')**2-np.linalg.norm(np.diag(T))**2)
+    if return_db:
+        SIRV = 10*np.log10(sigpow/intpow)
+
+    return SIRV,rankP
+
+@njit(fastmath = True)
+def accumu(lis):
+    """
+    Cumulative Sum. Same as np.cumsum()
+    """
+    result = np.zeros_like(lis)
+    for i in range(lis.shape[1]):
+        result[:,i] = np.sum(lis[:,:i+1])
+
+    return result
+
+@njit(fastmath = True)
+def merge_sort(list_):
+    """
+    Sorts a list in ascending order.
+    Returns a new sorted list.
+    
+    Divide : Find the midpoint of the list and divide into sublist
+    Conquer : Recursively sort the sublists created in previous step
+    Combine : Merge the sorted sublists created in previous step
+    
+    Takes O(n log n) time.
+    """
+    
+    def merge(left, right):
+        """
+        Merges two lists (arrays), sorting them in the process.
+        Returns a new merged list
+        
+        Runs in overall O(n) time
+        """
+        
+        l = []
+        i = 0
+        j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                l.append(left[i])
+                i += 1
+            else:
+                l.append(right[j])
+                j += 1
+                
+        while i < len(left):
+            l.append(left[i])
+            i += 1
+            
+        while j < len(right):
+            l.append(right[j])
+            j += 1
+        
+        return l
+
+    def split(list_):
+        """
+        Divide the unsorted list at midpoint into sublists.
+        Returns two sublists - left and right
+        
+        Takes overall O(log n) time
+        """
+        
+        mid = len(list_) // 2
+        
+        left = list_[:mid]
+        right = list_[mid:]
+        
+        return left, right
+
+    if len(list_) <= 1:
+        return list_
+    
+    left_half, right_half = split(list_)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+    
+    return np.array(merge(left, right))
+
+# @njit
+# def ProjectVectortoL1NormBall(H):
+#     Hshape = H.shape
+#     lr = np.repeat(np.reshape((1/np.linspace(1, H.shape[1], H.shape[1]))))
+
 def generate_correlated_uniform_sources(R, range_ = [-1,1], n_sources = 5, size_sources = 500000):
     """
     R : correlation matrix
@@ -393,13 +670,3 @@ def generate_correlated_copula_sources(rho = 0.0, df = 4, n_sources = 5, size_so
     S = t.cdf(X, df).T
     return S
 
-def display_matrix(array):
-    data = ''
-    for line in array:
-        if len(line) == 1:
-            data += ' %.3f &' % line + r' \\\n'
-            continue
-        for element in line:
-            data += ' %.3f &' % element
-        data += r' \\' + '\n'
-    display(Math('\\begin{bmatrix} \n%s\end{bmatrix}' % data))
